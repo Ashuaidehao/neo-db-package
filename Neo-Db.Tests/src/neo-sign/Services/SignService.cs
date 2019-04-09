494a2637de2c848b1e51db56a55e206358582d36
Shine <bfshm@qq.com>
2019-04-09 17:07:50 +0800

    SignService

---
+++ Neo-Db.Tests/src/neo-sign/Services/SignService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anemonis.AspNetCore.JsonRpc;
+using Neo.Cryptography;
+using Neo.Wallets;
+
+namespace Neo.Services
+{
+    [JsonRpcRoute("/api")]
+    public class SignService : IJsonRpcService
+    {
+
+        [JsonRpcMethod("createsign",0,1)]
+        public async Task<string > SignAsync(string txData,string privateKey)
+        {
+            var data = txData.HexToBytes();
+            var keypair = new KeyPair(privateKey.HexToBytes());
+            var signData = Crypto.Default.Sign(data, keypair.PrivateKey, keypair.PublicKey.EncodePoint(false).Skip(1).ToArray());
+            return signData.ToHexString();
+        }
+
+    }
+}
---
+++ Neo-Db.Tests/src/neo-sign/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using Anemonis.AspNetCore.JsonRpc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Neo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
+            services.AddResponseCompression(options =>
+            {
+                // options.EnableForHttps = false;
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json-rpc" });
+            });
+
+            services.AddJsonRpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseJsonRpc();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
